d654021eac23ce4501ba2c4163ef54b054576406
M.K <miika.kallasoja@gmail.com>
2020-09-13 10:53:00 +0300

    Test 5.14 (first version)

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -2,32 +2,68 @@
 
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
+import Togglable from './Togglable'
 
 
-test('renders content', () => {
-    const blog = {
-        id: 1,
-        title: 'My Test Blog',
-        author: 'Miika Kallasoja',
-        user: {
-            username: 'mijuoska', id: 1
-        }
-    }
+describe('Blog listing', () => {
+
+ const blog = {
+     id: 1,
+     title: 'My Test Blog',
+     author: 'Miika Kallasoja',
+     url: 'test.com',
+     likes: 1,
+     user: {
+         username: 'mijuoska',
+         id: 1
+     }
+ }
 
 
-    const user = {
-        id: 1
-    }
+ const user = {
+     id: 1
+ }
 
 
-    const component = render(
+
+
+test('Title and author are displayed', () => {
+   const component = render(
         <Blog blog={blog} user={user}/>
-    )
-
+           )
 
     expect(component.container).toHaveTextContent(
         'My Test Blog by Miika Kallasoja'
     )
-});
+
+     expect(component.container).toHaveTextContent(
+         'URL'
+     )
+})
+
+
+// test('URL and likes are displayed on click', () => {
+//       const component = render(
+//         <Blog blog={blog} user={user}/>
+//            )
+
+
+// component.debug()
+
+// // const button = component.getByText('Show');
+// // fireEvent.click(button);
+
+// expect(component.container).toHaveTextContent(
+//     'URL: test.com'
+// )
+
+// expect(component.container).toHaveTextContent(
+//     'Likes: 1'
+// )
+
+// })
+
+})
+
--- src/components/Togglable.js
+++ src/components/Togglable.js
@@ -23,7 +23,7 @@ return {
       <div style={hideWhenVisible}>
         <button onClick={toggleVisibility}>{props.buttonLabel}</button>
       </div>
-      <div style={showWhenVisible}>
+      <div style={showWhenVisible} className="togglableContent">
         {props.children}
         <button onClick={toggleVisibility}>Hide</button>
       </div>
