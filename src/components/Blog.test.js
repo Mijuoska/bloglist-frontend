f825cf1e80752d1ebbb5289f811c7e40f57af069
M.K <miika.kallasoja@gmail.com>
2020-09-13 16:21:31 +0300

    Test 5.16*

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -4,8 +4,7 @@ import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
-import Togglable from './Togglable'
-
+import BlogForm from './BlogForm'
 
 describe('Blog listing', () => {
 let component
@@ -57,7 +56,7 @@ test('URL and likes are displayed on click', () => {
 const div = component.container.querySelector('.togglableContent')
 
 const button = component.getByText('Show');
-fireEvent.click(button);
+fireEvent.click(button)
 
 expect(div).not.toHaveStyle(
     'display: none'
@@ -72,10 +71,9 @@ test('Clicking the Like button twice will call the event handler twice', () => {
         <Blog blog={blog} user={user} likeBlog={mockHandler}/>
            )
 
-    const button = component.getByText('Like');
-    fireEvent.click(button);
-    fireEvent.click(button);
-
+    const button = component.getByText('Like')
+    fireEvent.click(button)
+    fireEvent.click(button)
 
 
     expect(mockHandler.mock.calls).toHaveLength(2)
@@ -83,6 +81,39 @@ test('Clicking the Like button twice will call the event handler twice', () => {
 
 })
 
+// Tee uuden blogin luomisesta huolehtivalle lomakkelle testi, 
+// joka varmistaa, että lomake kutsuu propseina saamaansa
+//  takaisinkutsufunktiota oikeilla tiedoilla siinä vaiheessa kun blogi luodaan.
+test('Blog form is submitted with correct info', () => {
+    const createBlog = jest.fn()
+    const component =  render(<BlogForm createBlog={createBlog}/>)
+    const inputTitle = component.container.querySelector('#title')
+    const inputAuthor = component.container.querySelector('#author')
+    const inputURL = component.container.querySelector('#url')
+    const form = component.container.querySelector('form')
+
+    fireEvent.change(inputTitle, 
+        {target: {value: "This is a test created by Jest"}
+    })
+      fireEvent.change(inputAuthor, {
+          target: { value: "Jest"}
+      })
+    
+        fireEvent.change(inputURL, 
+        {target: {value: "jest.com"}
+        })
+
+    fireEvent.submit(form)
+    
+    expect(createBlog.mock.calls).toHaveLength(1);
+    expect(createBlog.mock.calls[0][0].title).toBe("This is a test created by Jest")
+    expect(createBlog.mock.calls[0][0].author).toBe("Jest")
+    expect(createBlog.mock.calls[0][0].url).toBe("jest.com")
+
+
+
+
+})
 
 })
 
--- src/components/BlogForm.js
+++ src/components/BlogForm.js
@@ -22,13 +22,13 @@ const BlogForm = ( { createBlog } ) => {
     <div>
       <form onSubmit={handleBlogSubmit}>
         <div>
-  Title: <input type="text" name="Title" value={title} onChange={({ target }) => setTitle(target.value)}/>
+  Title: <input type="text" name="Title" id="title" value={title} onChange={({ target }) => setTitle(target.value)}/>
         </div>
         <div>
-     Author: <input type="text" name="Author" value={author}  onChange={({ target }) => setAuthor(target.value)} />
+     Author: <input type="text" name="Author" id="author" value={author}  onChange={({ target }) => setAuthor(target.value)} />
         </div>
         <div>
-     Url: <input type="text" name="Url" value={url} onChange={({ target }) => setUrl(target.value)}/>
+     Url: <input type="text" name="Url" id="url" value={url} onChange={({ target }) => setUrl(target.value)}/>
         </div>
         <button type="submit">Create</button>
       </form>
